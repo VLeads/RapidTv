2576778d0a235f41c9f52ae85662824b8470e1a7
Vishal Kumar <vishal.kumar@benchwork.in>
2022-07-05 01:27:37 +0530

    fix typo

--- src/pages/error-404/Error404.jsx
+++ src/pages/error-404/Error404.jsx
@@ -11,8 +11,8 @@ export function Error404() {
         OOPS ! seems like you have come to wrong page !!
       </div>
       <img src="https://indianmemetemplates.com/wp-content/uploads/Bhai-kya-kar-raha-hai-tu-1024x711.jpg" />
-      <button className={styles.goBack} onClick={() => navigate("/")}>
-        GO TO HOMEPAGE
+      <button className={` ${styles.goBack}`} onClick={() => navigate("/")}>
+        GO BACK TO HOMEPAGE
       </button>
     </div>
   );
